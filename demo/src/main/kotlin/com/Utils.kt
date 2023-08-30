a152f418fe3d1ea28b1171d70f9710510476922b
kidar2 <kidar238@gmail.com>
2023-08-30 23:23:06 +0500

    init

---
+++ demo/src/main/kotlin/com/Utils.kt
@@ -0,0 +1,132 @@
+package com
+
+import java.util.*
+
+object Utils {
+
+    /**
+     * На вход строка с различным форматированием числа с валютой,
+     * разделителем разрядов или разделителем целой и дробной части
+     * Не допускаются разделители разрядов в дробной части
+     * Не допускается несколько валют
+     */
+    fun parseNumber(str: String): Double? {
+
+        var str = str.trim()
+
+        var hasSpec = false;
+
+        listOf('₽', '€', '$', '%').forEach {
+            if (str.startsWith(it)) {
+                if (hasSpec)
+                    return null;
+                hasSpec = true
+                str = str.substring(1, str.length)
+            }
+
+            if (str.endsWith(it)) {
+                if (hasSpec)
+                    return null;
+                hasSpec = true
+                str = str.substring(0, str.length - 1)
+            }
+        }
+
+        str = str.trim()
+
+        var isPlus = true;
+
+        if (str.startsWith('-')) {
+            isPlus = false
+            str = str.substring(1, str.length)
+        } else if (str.startsWith('+')) {
+            str = str.substring(1, str.length)
+        }
+
+        if (str.isEmpty())
+            return null;
+
+        val digits = LinkedList<Char>();
+        val fractionDigits = LinkedList<Char>();
+
+        var thousandSeparator: Char? = null
+        var decimalSeparator: Char? = null
+        var currentDigits = fractionDigits
+        var prevIsDigit = false;
+        for (n in str.length - 1 downTo 0)
+        {
+            val it = str[n];
+            when (it) {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
+                    currentDigits.add(0, it);
+                    prevIsDigit = true
+                }
+                ',', '.' -> {
+
+                    if (!prevIsDigit)
+                        return null
+
+                    if (decimalSeparator == null) {
+                        decimalSeparator = it;
+                        currentDigits = digits;
+
+                        if (thousandSeparator != null)
+                            return null;
+                    }
+                    else if (decimalSeparator == it)
+                        return null;
+                    else {
+                        if (thousandSeparator == null) {
+                            if (currentDigits.isNotEmpty() && currentDigits.size % 3 == 0)
+                                thousandSeparator = it;
+                            else
+                                return null;
+                        }
+                        else if (thousandSeparator != it)
+                            return null;
+                        else if (currentDigits.isEmpty() && currentDigits.size % 3 != 0)
+                            return null;
+                    }
+                    prevIsDigit = false;
+                }
+                ' ' -> {
+                    if (!prevIsDigit)
+                        return null
+                    if (thousandSeparator == null)
+                    {
+                        if (currentDigits.isNotEmpty() && currentDigits.size % 3 == 0)
+                            thousandSeparator = it;
+                        else
+                            return null;
+                    }
+                    else if (thousandSeparator != it)
+                        return null;
+                    else if (currentDigits.isEmpty() && currentDigits.size % 3 != 0)
+                        return null;
+                    prevIsDigit = false;
+                }
+
+                else -> return null;
+            }
+        }
+
+        if (digits.isEmpty() && fractionDigits.isEmpty())
+            return null;
+
+
+        val res = if (digits.isEmpty()) {
+            if (currentDigits.isEmpty())
+                ".${fractionDigits.joinToString("")}".toDouble()
+            else
+                currentDigits.joinToString("").toDouble();
+        }
+        else
+            "${digits.joinToString("")}.${fractionDigits.joinToString("")}".toDouble()
+
+        var r = if (isPlus) res else -res;
+
+        println(r)
+
+        return r;
+    }
+}
---
+++ demo/src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -0,0 +1,48 @@
+package com.example.demo
+
+import com.Utils
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.util.NumberUtils.parseNumber
+
+
+class DemoApplicationTests {
+
+    @Test
+    fun parseNumberTest() {
+
+        Assertions.assertNotNull(Utils.parseNumber("-666 ₽"))
+        Assertions.assertNotNull(Utils.parseNumber("+666 ₽"))
+        Assertions.assertNotNull(Utils.parseNumber("123,123,666.3 ₽"))
+        Assertions.assertNotNull(Utils.parseNumber("$+332,122"))
+        Assertions.assertNotNull(Utils.parseNumber("123 332,122"))
+        Assertions.assertNotNull(Utils.parseNumber("123 332,122666666666666666322"))
+        Assertions.assertNotNull(Utils.parseNumber("1 123 332,12266322"))
+        Assertions.assertNotNull(Utils.parseNumber("€-332,122"))
+        Assertions.assertNotNull(Utils.parseNumber("33.2 ₽"))
+        Assertions.assertNotNull(Utils.parseNumber("332122,00%"))
+        Assertions.assertNotNull(Utils.parseNumber("332,122.00"))
+        Assertions.assertNotNull(Utils.parseNumber(".011"))
+        Assertions.assertNotNull(Utils.parseNumber("$.011"))
+        Assertions.assertNotNull(Utils.parseNumber("-0"))
+
+        Assertions.assertNull(Utils.parseNumber("1,121 332.122 ₽"))
+        Assertions.assertNull(Utils.parseNumber("--20"))
+        Assertions.assertNull(Utils.parseNumber("++10"))
+        Assertions.assertNull(Utils.parseNumber("1,332 122 ₽"))
+        Assertions.assertNull(Utils.parseNumber("-332€122"))
+        Assertions.assertNull(Utils.parseNumber("$332,122 ₽"))
+        Assertions.assertNull(Utils.parseNumber("$1,1,1,1.2"))
+        Assertions.assertNull(Utils.parseNumber("$1,1,1,1 2"))
+        Assertions.assertNull(Utils.parseNumber("$1,1 112"))
+        Assertions.assertNull(Utils.parseNumber("$1,1,112"))
+        Assertions.assertNull(Utils.parseNumber("$$1.011"))
+        Assertions.assertNull(Utils.parseNumber("1.011₽₽"))
+        Assertions.assertNull(Utils.parseNumber("1."))
+        Assertions.assertNull(Utils.parseNumber("."))
+        Assertions.assertNull(Utils.parseNumber("1 . 1"))
+        Assertions.assertNull(Utils.parseNumber("1 . 111"))
+    }
+
+}
